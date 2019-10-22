0d0c3fb0592169e213d0e989619ca67748c3406b
markbain <markbaindesign@gmail.com>
2019-10-22 18:10:03 +0200

    add gl and ghow git aliases

--- oh-my-zsh/alias.zsh
+++ oh-my-zsh/alias.zsh
@@ -13,10 +13,16 @@ alias vssh='vagrant ssh'
     alias ssh_ruby_prod='ssh wp_xkwiqr@67.205.16.93' # SSH to production server
     alias ssh_ruby_stag='ssh wp_jxdneh@208.113.143.141' # SSH to staging server
     alias ssh_avdicodes='ssh wp_56wdp4@avdi.codes.dream.website'
-    
+
 # Misc
 
 alias hud='screenfetch'
 alias dnsflush='sudo /etc/init.d/nscd restart' # Clear the DNS cache
 alias go='gnome-open' # Open a file in another application
 alias top10='sudo du -hsx * | sort -rh | head -10' # Show the 10 largest dirs or files
+
+# Git 
+alias ghow='git show --stat --pretty=format:"%C(yellow)%h%C(reset) %G? %C(bold blue)%an%C(reset) %s%C(bold cyan)%d%C(reset) %C(green)%cr.%C(reset) %n%n%b%n%N%-%n"
+'
+alias gl='git log --graph --pretty=format:"%C(yellow)%h%C(reset) %G? %C(bold blue)%an%C(reset) %s%C(bold cyan)%d%C(reset) %C(green)%cr.%C(reset)"
+'
